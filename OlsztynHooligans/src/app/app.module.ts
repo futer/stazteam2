39b2e2201a5f0fd1fa5407dc44b7a016f0999af6
Adrian Barszcz <adrian.barszcz@billennium.com>
2018-12-19 11:25:44 +0100

    Dodawanie componentów

--- OlsztynHooligans/src/app/app.module.ts
+++ OlsztynHooligans/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DocumentComponent } from './document/document.component';
+import { ChatComponent } from './chat/chat.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    EditUserComponent,
+    DashboardComponent,
+    DocumentComponent,
+    ChatComponent,
+    BookmarksComponent
   ],
   imports: [
     BrowserModule,
