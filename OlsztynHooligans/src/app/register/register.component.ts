13343705969aa43180c9aba2636dc4db5c45041f
Adrian Barszcz <adrian.barszcz@billennium.com>
2019-01-09 14:36:20 +0100

    capture data from inputs using eventEmiiter and  function postuser

--- OlsztynHooligans/src/app/register/register.component.ts
+++ OlsztynHooligans/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { IUser } from '../interface/interface.IUser';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { AuthorizationDataService} from '../service/authorization-data.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,12 +10,55 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private dataService: AuthorizationDataService) { }
+
+  user1: IUser;
+  userForm: FormGroup;
+  email: string;
+  password: string;
+  passwordconfirm: string;
 
   ngOnInit() {
+    this.user1 = { email: '', password: '', name: '', surname: '' };
+    this.passwordconfirm = '';
+    this.userForm = this.formBuilder.group({
+      'email': [''],
+      'password': [''],
+      'name': [''],
+      'surname': ['']
+    });
   }
 
-  getLogin() {
+  // eventEmitter function
+
+  onChangeEmail(value) {
+    console.log(this.user1.email);
+    this.user1.email = value;
+  }
+
+  onChangeName(value) {
+    console.log(this.user1.name);
+    this.user1.name = value;
+  }
+
+  onChangeSurname(value) {
+    console.log(this.user1.surname);
+    this.user1.surname = value;
+  }
+
+  onChangePassword(value) {
+    console.log(this.user1.password);
+    this.user1.password = value;
+  }
+
+  onChangePassword2(value) {
+    console.log(this.passwordconfirm);
+    this.passwordconfirm = value;
+  }
+  // Used in validation
+  get f() { return this.userForm.controls; }
+
+  onSubmit() {
     this.router.navigate([`/login`]);
   }
 }
