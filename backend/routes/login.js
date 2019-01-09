91c3ba3434430a85c48b47676e0b04a041e74e75
Adrian Barszcz <adrian.barszcz@billennium.com>
2019-01-09 08:21:51 +0100

    router for login and register

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const admin = require('firebase-admin');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('login page');
 });
 
 module.exports = router;
+
+router.post('/', function (req, res, next) {
+  // admin.database().ref('data/users').orderByChild('email').equalTo(req.body.email).on("value", function (snapshot) {
+  // });
+  admin.database().ref('data/users').findOne({ email: req.body.email, password: req.body.password }, (err, data) => {
+    if (err) {
+      return res.status(500).send('Błąd danych');
+    }
+    if (data) {
+      const token = Helper.createJWT(data);
+      //console.log(token);
+      return res.status(200).send({ message: 'Ok' });
+    } else {
+      return res.status(400).send({ message: 'Nie odnaleziono danych' })
+    }
+  });
+})
+
+
+// ref.child('users').orderByChild('name').equalTo('John Doe').on("value", function(snapshot) {
+//   console.log(snapshot.val());
+//   snapshot.forEach(function(data) {
+//       console.log(data.key);
+//   });
+// });
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const admin = require('firebase-admin');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('register page');
 });
-
+router.post('/', function (req, res, next) {
+  if (result.error) {
+    return res.status(400).send({ message: result.error.message });
+  }
+  //admin.database().ref('/messages').push({original: original})
+  //const usersRef = ref.child("users");
+  admin.database().ref('data/users').push({
+    email: req.body.email,
+    name: req.body.name,
+    surname: req.body.surname,
+    password: req.body.password
+  });
+})
 module.exports = router;
