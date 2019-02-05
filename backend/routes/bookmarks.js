cf5c60d5abf139fac8e3a7280aac8253fc69b276
Hubert Koloszewski <hubert.koloszewski@billennium.com>
2019-02-05 09:56:17 +0100

    adding bookmark fix

--- backend/routes/bookmarks.js
+++ backend/routes/bookmarks.js
@@ -32,8 +32,15 @@ router.post('/', function (req, res, next) {
   admin.database().ref(`data/bookmarks`).child(req.body.title).set({
     title: req.body.title,
     text: req.body.text
+  }).then(function (bookmark) {
+    console.log("Successfully created new bookmark");
+    return res.status(200).send({ message: 'Ok' });
+
+  })
+  .catch(function (error) {
+    console.log("Error creating new bookmark:", error);
+    return res.status(400).send({ message: 'Not Ok'})
   });
-  res.status(200).send({ message: 'Ok' });
 })
 
 router.get('/', function (req, res, next) {
@@ -46,7 +53,6 @@ router.get('/', function (req, res, next) {
   }, (errorObject) => {
     console.log("The read failed: " + errorObject.code);
   })
-
 }),
  
 module.exports = router;
