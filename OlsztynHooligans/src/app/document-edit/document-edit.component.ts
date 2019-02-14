37ca476df184642862ef836deb00c2517a48aae8
Maciej Lamperski <maciej.lamperski@billennium.com>
2019-02-14 12:01:30 +0100

    packages + document view

--- OlsztynHooligans/src/app/document-edit/document-edit.component.ts
+++ OlsztynHooligans/src/app/document-edit/document-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-document-edit',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.scss']
 })
 export class DocumentEditComponent implements OnInit {
-
+  public Editor = ClassicEditor;
   constructor() { }
 
   ngOnInit() {
